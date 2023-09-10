c7fde78c9b573f59e2e8e725c836913f52e49f63
Maor Ovadya <maorovadya@icloud.com>
2023-09-09 18:52:27 -0700

    fix error

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,7 +1,8 @@
 //TITLE
 import Title from '../components/Title'
 
-const webSitePicture = 'https://teddies-vs-monsters-frontend.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fhero.b9dd666f.png&w=3840&q=75'
+import sitePic from '../assets/images/sitePic.webp'
+ 
 function Projects() {
 
   return (
@@ -12,7 +13,7 @@ function Projects() {
           {projects.map((pro) => 
             <div key={pro.id} className='mx-5 border-black rounded-xl border w-[27rem] h-full'>
               <div className='flex flex-col bg-slate-300 capitalize rounded-xl text-black relative'>
-                <img className='rounded-t-xl' src={pro.projectImage}/>
+                <img className='rounded-t-xl' src={sitePic}/>
                   <div className='pt-3 leading-6 text-left px-3 border-black border-b pb-3'>
                     {pro.text}
                   </div>
@@ -31,11 +32,9 @@ function Projects() {
 
 export default Projects
 
-
 const projects = [ 
   {
     id: 1,
-    projectImage: [webSitePicture],
     text: `Teddies Vs Monster e-commerce platform for gaming board .`,
     stack: 'NextJs \n,TypeScript \n,Tailwind Css \n,swiper \n,Zuztand \n,strapi \n,Strip \n,Figma \n,Trello',
     link: 'https://teddies-vs-monsters-frontend.vercel.app,'
