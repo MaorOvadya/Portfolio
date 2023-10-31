d2850c87ae375a713f55e7718e8585833d61b236
Maor Ovadya <maorovadya@icloud.com>
2023-10-31 14:13:31 -0700

    add project

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,7 +1,8 @@
 //TITLE
 import Title from "../components/Title";
 
-import sitePic from "../assets/images/sitePic.webp";
+import sitePicT from "../assets/images/sitePicT.webp";
+import sitePicl from "../assets/images/sitePicL.png";
 
 function Projects() {
   return (
@@ -46,14 +47,73 @@ function Projects() {
                         See Project
                       </a>
                     </button>
+                    <button className="bg-slate-400 text-white w-[10rem] h-[2.5rem] rounded-md transition-all duration-500 ease-in hover:bg-white hover:text-amber-300 hover:border hover:border-slate-400">
+                      <a
+                        className="pb-10 pt-10"
+                        target="_blank"
+                        href={pro.TeddiesAndMonsterGithub}>
+                        Github
+                      </a>
+                    </button>
                   </div>
                 </div>
 
                 <div className="">
                   <img
                     className="z-40 w-fit md:w-[30rem] rounded-md"
-                    src={sitePic}
+                    src={sitePicT}
                   />
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col capitalize w-full ">
+              <p className="px-5 text-center capitalize text-2xl pb-10 pt-10">
+                {pro.LandingPageTitle}
+              </p>
+              <div className="flex flex-wrap-reverse xl:flex-wrap justify-evenly p-10">
+                <div className="">
+                  <img
+                    className="z-40 w-fit md:w-[30rem] rounded-md"
+                    src={sitePicl}
+                  />
+                </div>
+                <div className="flex flex-wrap py-5 w-[30rem] text-left leading-8">
+                  {pro.LandingPageText.split("\n").map((t, index) => (
+                    <div
+                      key={index}
+                      className=" bg-gradient-to-tr from-blue-300 to-white self-center p-3 rounded-md">
+                      <p
+                        key={index}
+                        className="capitalize text-base font-extralight">
+                        {t}
+                      </p>
+                    </div>
+                  ))}
+                  <div className="capitalize text-base font-extralight py-5">
+                    {pro.LandingPageStack.split("\n").map((s, index) => (
+                      <p className="text-left" key={index}>
+                        {s}
+                      </p>
+                    ))}
+                  </div>
+                  <div className="flex flex-col items-center gap-5 sm:flex-row justify-between w-full ">
+                    <button className="bg-slate-400 text-white w-[10rem] h-[2.5rem] rounded-md transition-all duration-500 ease-in hover:bg-white hover:text-blue-300 hover:border hover:border-slate-400">
+                      <a
+                        className="pb-10 pt-10"
+                        target="_blank"
+                        href={pro.LandingPageLink}>
+                        See Project
+                      </a>
+                    </button>
+                    <button className="bg-slate-400 text-white w-[10rem] h-[2.5rem] rounded-md transition-all duration-500 ease-in hover:bg-white hover:text-blue-300 hover:border hover:border-slate-400">
+                      <a
+                        className="pb-10 pt-10"
+                        target="_blank"
+                        href={pro.LandingPageGithub}>
+                        Github
+                      </a>
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
@@ -73,5 +133,13 @@ const projects = [
     TeddiesAndMonsterText: `"E-Commerce Platform For Gaming Board: Teddies vs Monsters Board Game is an engaging and fun board game where players take on the roles of teddy bears defending their realm against menacing monsters.`,
     TeddiesAndMonsterStack: `Tech Stack:\n#Trello,#Figma,#TypeScript,#Tailwind Css,#NextJs,\n#Zuztand,#strapi,#Strip,#swiper,`,
     TeddiesAndMonsterLink: "https://teddies-vs-monsters-frontend.vercel.app/",
+    TeddiesAndMonsterGithub:
+      "https://github.com/chicacode/teddies-vs-monsters-frontend",
+
+    LandingPageTitle: `Life Airline`,
+    LandingPageText: `"Life Airline is a Static responsive website contaion only structure and Css.`,
+    LandingPageStack: `Tech Stack:\n#Html,#Sass`,
+    LandingPageLink: "https://main--lifeairline.netlify.app",
+    LandingPageGithub: "https://github.com/MaorOvadya/landingPage",
   },
 ];
